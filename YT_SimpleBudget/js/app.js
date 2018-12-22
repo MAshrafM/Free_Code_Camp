44ad2b14328bab147439ded7ec4510201436e25e
MAshraf <ee.mashraf@gmail.com>
2018-12-22 13:15:37 +0200

    simple calculator

--- YT_SimpleBudget/js/app.js
+++ YT_SimpleBudget/js/app.js
@@ -15,4 +15,133 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
+  // submit budget method
+  submitBudgetForm(){
+    const value = this.budgetInput.value;
+    if(value === '' || value < 0){
+      this.budgetFeedback.classList.add('showItem');
+      this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
+      const self = this;
+      setTimeout(function(){
+        self.budgetFeedback.classList.remove('showItem');
+      }, 4000)
+    } else {
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = "";
+      this.showBalance();
+    }
+  }
+
+  // Show Balance
+  showBalance(){
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent) - expense;
+    this.balanceAmount.textContent = total;
+
+    if(total < 0){
+      this.balance.classList.remove('showGreen', 'showBlack');
+      this.balance.classList.add('showRed');
+
+    } else if (total > 0){
+      this.balance.classList.remove('showRed', 'showBlack');
+      this.balance.classList.add('showGreen');
+    } else {
+      this.balance.classList.remove('showRed', 'showGreen');
+      this.balance.classList.add('showBlack');
+    }
+  }
+
+  // Submit Expense Form
+  submitExpenseForm(){
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+
+    if(expenseValue === '' || amountValue === '' || amountValue < 0){
+      this.expenseFeedback.classList.add('showItem');
+      this.expenseFeedback.innerHTML = `<p>Values cannot ne empty or negative.</p>`;
+      const self = this;
+      setTimeout(function(){
+        self.expenseFeedback.classList.remove('showItem');
+      }, 4000);
+    } else {
+      let amount = parseInt(amountValue);
+      this.expenseInput.value = '';
+      this.amountInput.value = '';
+
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount: amount
+      }
+      this.itemID++;
+      this.itemList.push(expense);
+
+      this.addExpense(expense);
+      this.showBalance();
+    }
+  }
+
+  // Add Expense
+  addExpense(expense){
+    const div = document.createElement('div');
+    div.classList.add('expense');
+    div.innerHTML = `
+      <div class="expense-item d-flex justify-content-between align-items-baseline">
+        <h6 class="expense-title mb-0 text-uppercase list-item">${expense.title}</h6>
+        <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+        <div class="expense-icons list-item">
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+            <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon mx-2" data-id="${expense.id}">
+            <i class="fas fa-trash"></i>
+          </a>
+        </div>
+      </div>
+    `;
+    this.expenseList.appendChild(div);
+  }
+
+  // Total Expenses
+  totalExpense(){
+    let total = 0;
+    if(this.itemList.length > 0){
+      total = this.itemList.reduce(function(acc, curr){
+        acc += curr.amount;
+        return acc;
+      }, 0)
+    }
+    this.expenseAmount.textContent = total;
+    return total;
+  }
 }
+
+function eventListeners(){
+  const budgetForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById('expense-form');
+  const expenseList = document.getElementById('expense-list');
+
+  const ui = new UI();
+
+  // Budget Form Submit
+  budgetForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    ui.submitBudgetForm();
+  })
+
+  // Expense Form Submit
+  expenseForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    ui.submitExpenseForm();
+  })
+
+  // Budget Form Submit
+  expenseList.addEventListener('click', function(e){
+    
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  eventListeners();
+})
