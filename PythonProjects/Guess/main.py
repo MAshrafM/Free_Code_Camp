dd0e6e232204ae7aaf571381a9ef217d43d1c8f9
MAshrafM <ee.mashraf@gmail.com>
2021-02-19 00:51:08 +0200

    computer guess game

--- PythonProjects/Guess/main.py
+++ PythonProjects/Guess/main.py
@@ -14,4 +14,24 @@ def guess(x):
 
     print(f'Yay, congrats. You have guessed the number {random_num}')
 
-guess(100)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Yay, congrats. You have guessed the number {guess}')
+
+#guess(100)
+computer_guess(100)
