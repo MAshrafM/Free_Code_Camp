68f9af4c0cb0241722c352572e470fa888c38784
MAshrafM <ee.mashraf@gmail.com>
2021-03-01 13:08:20 +0200

    gameify hangman

--- PythonProjects/Hangman/hangman.py
+++ PythonProjects/Hangman/hangman.py
@@ -19,9 +19,11 @@ def hangman():
     # keep used letters
     used_letters = set()
 
-    while len(word_letters) > 0:
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
         #letters used
-        print("You have used these letters: ", ' '.join(used_letters))
+        print("You have ", lives, " Lives left and You have used these letters: ", ' '.join(used_letters))
         #show current state
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print("Current word: ", ' '.join(word_list))
@@ -31,9 +33,18 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+
+            else:
+                lives = lives - 1
+                print("Letter is not in the word")
         elif user_letter in used_letters:
             print("You have alreadu used that character, Please try again.")
         else:
             print("Invalid character. Please try again")
+
+    if lives == 0:
+        print("You died, the word was ", word)
+    else:
+        print("You guessed the word", word, "!!")
 
 hangman()
