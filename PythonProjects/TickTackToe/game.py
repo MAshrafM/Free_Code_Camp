4f2326b04287c6820c87395411208a96eef56343
MAshrafM <ee.mashraf@gmail.com>
2021-03-07 16:44:47 +0200

    game winner

--- PythonProjects/TickTackToe/game.py
+++ PythonProjects/TickTackToe/game.py
@@ -29,7 +29,32 @@ class TicTacToe:
         # if valid make move and return true, if not false
         if self.board[square] == ' ':
             self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
             return True
+        return False
+
+    def winner(square, letter):
+        # winner if 3 in a row, colm or diagonal
+        # check row
+        row_ind = square // 3
+        row = self.board[row_ind*3 : (row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+        # check col
+        col_ind = square % 3
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+        # check diagonal
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            if all([spot == letter for spot in diagonal2]):
+                return True
+
         return False
 
 def play(game, x_player, o_player, print_game=True):
@@ -50,5 +75,13 @@ def play(game, x_player, o_player, print_game=True):
                 game.print_board()
                 print('')
 
+            if game.current_winner:
+                if print_game:
+                    print(letter + ' wins!')
+                return letter
+
             letter = 'O' if letter == 'X' else 'X' #switch player
+
+        if print_game:
+            print('It\'s a Tie!')
 
