b5fb621f36706d31d6ef215420b8916975f1175a
MAshrafM <ee.mashraf@gmail.com>
2021-03-23 17:18:41 +0200

    dig and board formating

--- PythonProjects/Minesweeper/game.py
+++ PythonProjects/Minesweeper/game.py
@@ -42,6 +42,63 @@ class Board:
                     continue
                 if self.board[r][c] == "*":
                     num_neighboring_bombs += 1
+
+    def dig(self, row, col):
+
+        if self.board[row][col] == '*':
+            return False
+        elif self.board[row][col] > 0:
+            return True
+
+        for r in range(max(0, row-1), min(self.dim_size-1, (row+1)+1)):
+            for c in range(max(0, col-1), min(self.dim_size-1, (col+1)+1)):
+                if (r, c) in self.dug:
+                    continue
+                self.dig(r, c)
+
+        return True
+
+    def __str__(self):
+        visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        for row in range(self.dim_size):
+            for col in range(self.dim_size):
+                if (row, col) in self.dug:
+                    visible_board[row][col] = str(self.board[row][col])
+                else:
+                    visible_board[row][col] = ' '
+
+        string_rep = ''
+        widths = []
+        for idx in range(self.dim_size):
+            columns = map(lambda x: x[idx], visible_board)
+            widths.append(
+                len(
+                    max(columns, key = len)
+                )
+            )
+        indices = [i for i in range(self.dim_size)]
+        indices_row = '   '
+        cells = []
+        for idx, col in enumerate(indices):
+            format = '%-' + str(widths[idx]) + "s"
+            cells.append(format % (col))
+        indices_row += '  '.join(cells)
+        indices_row += ' \n'
+
+        for i in range(len(visible_board)):
+            row = visible_board[i]
+            string_rep += f'{i} |'
+            cells = []
+            for idx, col in enumerate(row):
+                format = '%-' + str(widths[idx]) + "s"
+                cells.append(format % (col))
+            string_rep += ' |'.join(cells)
+            string_rep += ' |\n'
+
+        str_len = int(len(string_rep) / self.dim_size)
+        string_rep = indices_row + '-'*str_len + '\n' + string_rep + '-'*str_len
+
+        return string_rep
          
 def play(dim_size=10, num_bombs=10):
     """
@@ -50,5 +107,6 @@ def play(dim_size=10, num_bombs=10):
     - show board and prompt
     - gamefiy 
     """
+    board = Board(dim_size, num_bombs)
     pass
 
